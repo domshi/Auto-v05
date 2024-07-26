d0083f42a152c50bad45164fab7a2418bb42cb7a
domshi <90562421+domshi@users.noreply.github.com>
2024-07-26 01:01:28 +0000

    broken update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-AllowedVehiclesList = ['Ford F-150','Chevrolet Silverado','Tesla Cybertruck','Toyota Tundra','Nissan Titan','Rivian R1T','Ram 1500' ]
+#just looking at the txt file
+def read():
+  with open('Vehicles.txt', 'r') as db:
+    return db.read()
 
+#looking and adding to the txt file
+def write(obj):
+  with open('Vehicles.txt', 'a') as db:
+    return db.write('\n' + obj)
+
+#deleteing entry ??? to be continued
+def delete(obj):
+  with open('Vehicles.txt', 'w') as db:
+    return db.write(db
 
 
 while (True):
+  print(" ")
   print("********************************")
   print("AutoCountry Vehicle Finder v0.4")
   print("********************************")
@@ -14,41 +27,48 @@ while (True):
   print('4. DELETE Authorized Vehicle')
   print('5. Exit')
   selection = input("Please enter your choice: ")
+  print(" ")
   if not selection.isnumeric():
     print("Invalid Choice,Please select a valid choice from the menu")
     continue
   if int(selection) == 1:
-    print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
-    for vehicles in AllowedVehiclesList:
-      print(vehicles)
+    print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ')
+    print(read())
     continue
   if int(selection) == 2:
+    read()
     vehicleChoice = input('Please Enter the full Vehicle name: ')
-    if vehicleChoice in AllowedVehiclesList:
+    if vehicleChoice in read():
       print((vehicleChoice) + ' is an authorized vehicle')
-    if vehicleChoice not in AllowedVehiclesList:
+    if vehicleChoice not in read():
       print((vehicleChoice) + ' is not an authorized vehicle, if you received this in error please check the spelling and try again')
     continue
   if int(selection) == 3:
+    read()
     vehicleAdd = input('Please Enter the full Vehicle name you would like to add: ')
-    if vehicleAdd in AllowedVehiclesList:
+    if vehicleAdd in read():
       print((vehicleAdd) + ' is already an authorized vehicle')
-    if vehicleAdd not in AllowedVehiclesList:
-      AllowedVehiclesList.append(vehicleAdd)
+    if vehicleAdd not in read():
+      write(vehicleAdd)
       print('You have added ' + (vehicleAdd) + ' to the authorized vehicle list')
     continue
   if int(selection) == 4:
     vehicleDelete = input('Please Enter the full Vehicle name you would like to REMOVE: ')
-    if vehicleDelete not in AllowedVehiclesList:
+    read()
+    if vehicleDelete not in read():
       print((vehicleDelete) + ' is not an authorized vehicle')
-    if vehicleDelete in AllowedVehiclesList:
+    
+    
+    if vehicleDelete in read():
       confirmDelete = input('Are you sure you want to remove "' + (vehicleDelete) + '" from the Authorized Vehicles List? (Yes/No): ')
       if confirmDelete == 'No':
         continue
       if confirmDelete != 'Yes':
         print('Please enter Yes or No')
       if confirmDelete == 'Yes':
-        AllowedVehiclesList.remove(vehicleDelete)
+        
+        
+        delete(vehicleDelete)
         print('You have REMOVED "' + (vehicleDelete) + '" from the Authorized Vehicle List')
     continue
   if int(selection) != 5:
